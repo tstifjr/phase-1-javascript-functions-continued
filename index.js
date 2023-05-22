495313982f5933b244033572007c2b5e40dee098
TJ Stifter <tjstifter@gmail.com>
2023-05-22 19:04:10 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (activity="go to the office") => `This Monday, I will ${activity}.`
+
+function wrapAdjective(flair="*"){
+    return function(adj="special"){
+       return 'You are ' + flair + adj + flair + "!";
+    } 
+};
